2a40ddf6ba0ba99d5d903bd37af83ac754e7f2ba
Sepehr Hakami <sepehr.hakami@gmail.com>
2023-10-14 18:32:17 -0700

    use json data files

--- src/app/shb-resume/shb-resume.component.ts
+++ src/app/shb-resume/shb-resume.component.ts
@@ -1,19 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Education } from './education';
 import { Employment } from './employment';
-
-const EDUCATIONS: Education[] = [
-  { school: 'University of Washington', major: 'B.S. in Computer Science', dateFrom: '2015', dateTo: '2018' },
-  { school: 'Bellevue College', major: 'Associate in Arts and Sciences', dateFrom: '2012', dateTo: '2015' },
-];
-
-const EXPERIENCES: Employment[] = [
-  { company: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.' },
-  { company: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.' },
-  { company: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.' },
-  { company: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.' },
-  { company: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.' },
-];
+import * as EDUCATIONS from 'src/assets/education.json';
+import * as EMPLOYMENTS from 'src/assets/employment.json';
 
 @Component({
   selector: 'app-shb-resume',
@@ -25,11 +14,10 @@ export class ShbResumeComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void { }
 
   displayedColumns: string[] = ['data'];
-  tblEducationSource = EDUCATIONS;
-  tblExperienceSource = EXPERIENCES;
+  tblEducationSource: Education[] = Array.from(EDUCATIONS);
+  tblExperienceSource: Employment[] = Array.from(EMPLOYMENTS);
 
 }
